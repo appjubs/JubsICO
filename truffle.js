553e2d48516012b8eb1eb923edd2c5377dd3e243
Dkdaniz <dkdaniz@outlook.com>
2017-12-17 19:15:48 -0200

    alter name

--- truffle.js
+++ truffle.js
@@ -4,7 +4,7 @@ module.exports = {
       host: "localhost",
       port: 8545,
       From:0x42d1cd59ca117d82ab0b8fe26d8d5a602d30939e,
-      network_id: "*", // Match any network id
+      network_id: "3", // Match any network id
       gas: 4612388
     },
     
