4b522f4074d4f87b738e687b3f70a44116135d9f
Dkdaniz <dkdaniz@outlook.com>
2017-12-17 18:07:34 -0200

    Correction

--- contracts/JubsICO.sol
+++ contracts/JubsICO.sol
@@ -1,12 +1,5 @@
 pragma solidity ^0.4.15;
 
-/**
- * @title JubsICO
- * @Autor Daniel Marques (Dkdaniz)
- * @dev see https://github.com/appjubs/JubsICO/blob/master/contracts/JubsICO.sol
- */
-
-
 import './ERC20Basic.sol';
 import './StandardToken.sol';
 
@@ -14,101 +7,140 @@ contract JubsICO is StandardToken {
     using SafeMath for uint256;
 
     //Information coin
-    string public name = "Jubs Coin Service";
-    string public symbol = "JCS";
+    string public name = "Honor";
+    string public symbol = "HNR";
     uint256 public decimals = 18;
-    uint256 public totalSupply = 300000000 * (10*7) * (10 ** decimals); //300 000 000 JCS
+    uint256 public totalSupply = 10 * (10*7) * (10 ** decimals); //100 000 000 HNR
 
-    //Adress informated in white paper
-    address public wallet;
-    address public investorsWallet;                     //3%
-    address public workingCapitalWallet;                //10%
-    address public walletToken;                         //70%    
+    //Adress informated in white paper 
+    address public walletETH;                           //Wallet ETH
+    address public icoWallet;                           //63%
+    address public preIcoWallet;                        //7%
     address public teamWallet;                          //10%
-    address public rewardWallet;                        //5%
-    address public advisior;                            //2%        
+    address public bountyMktWallet;                     //7%
+    address public arbitrationWallet;                   //5%
+    address public rewardsWallet;                       //5%
+    address public advisorsWallet;                      //2%
+    address public operationWallet;                     //1%       
 
-    //Utils ICO
+    //Utils ICO   
+    uint256 public icoStage = 0;        
     uint256 public tokensSold = 0;                      // total number of tokens sold
     uint256 public totalRaised = 0;                     // total amount of money raised in wei
     uint256 public totalTokenToSale = 0;
-    uint256 public rate = 350;                          // JCS/ETH rate
-    uint256 public icoEtherMinCap;                      // in wei
-    uint256 public icoEtherMaxCap;                      // in wei
+    uint256 public rate = 3500;                         // HNR/ETH rate / initial 50%
     bool public pauseEmergence = false;                 //the owner address can set this to true to halt the crowdsale due to emergency
+    
 
     //Time Start and Time end
-    uint256 public icoStartTimestamp = 1484179199;      //01/11/2017 @ 11:59pm (UTC)
-    uint256 public icoEndTimestamp = 1512000000;        //30/11/2017 @ 11:59pm (UTC)
-    uint256 public teamEndTimestamp = 1572566400;       //11/01/2019 @ 12:00am (UTC) end of the waiting time for the team to withdraw 
-                                                        //their coins exactly 2 years later
+    //Stage pre sale                                            //50%
+    uint256 public icoStartTimestampStage = 1515974400;         //15/01/2018 @ 00:00am (UTC)
+    uint256 public icoEndTimestampStage = 1518911999;           //18/02/2018 @ 11:59pm (UTC)
+
+    //Stage 1                                                   //25%
+    uint256 public icoStartTimestampStage1 = 1518998400;        //19/02/2018 @ 00:00am (UTC)
+    uint256 public icoEndTimestampStage1 = 1519603199;          //25/02/2018 @ 11:59pm (UTC)
+
+    //Stage 2                                                   //20%
+    uint256 public icoStartTimestampStage2 = 1519603200;        //26/02/2018 @ 00:00am (UTC)
+    uint256 public icoEndTimestampStage2 = 1520207999;          //04/03/2018 @ 11:59pm (UTC)
+
+    //Stage 3                                                   //15%
+    uint256 public icoStartTimestampStage3 = 1520208000;        //05/03/2018 @ 00:00am (UTC)
+    uint256 public icoEndTimestampStage3 = 1520812799;          //11/03/2018 @ 11:59pm (UTC)
+
+    //Stage 4                                                   //10%
+    uint256 public icoStartTimestampStage4 = 1520812800;        //12/02/2018 @ 00:00am (UTC)
+    uint256 public icoEndTimestampStage4 = 1521417599;          //18/02/2018 @ 11:59pm (UTC)
+
+    //end of the waiting time for the team to withdraw 
+    uint256 public teamEndTimestamp = 1579046400;               //01/15/2020 @ 12:00am (UTC) 
+                                                                
 
 // =================================== Events ================================================
 
-    event Burn(address indexed burner, uint256 value);
+    event Burn(address indexed burner, uint256 value);  
 
 
 // =================================== Constructor =============================================
-    
+       
     function JubsICO (
-        address _wallet,
-        address _investorsWallet,
-        address _workingCapitalWallet,
-        address _walletToken,
-        address _teamWallet,
-        address _rewardWallet,
-        address _advisior,
-        uint256 _icoEtherMinCap,
-        uint256 _icoEtherMaxCap)
+        address  _icoWallet,
+        address  _preIcoWallet,
+        address  _teamWallet,
+        address  _bountyMktWallet,
+        address  _arbitrationWallet,
+        address  _rewardsWallet,
+        address  _advisorsWallet,
+        address  _operationWallet
+        )
         public 
         {
-            require(_wallet != 0x0);
-            require(_investorsWallet != 0x0);
-            require(_workingCapitalWallet != 0x0);
-            require(_walletToken != 0x0);
+            require(_icoWallet != 0x0);
+            require(_preIcoWallet != 0x0);
             require(_teamWallet != 0x0);
-            require(_rewardWallet != 0x0);
-            require(_advisior != 0x0);            
-            require(_icoEtherMinCap > 0);
-            require(_icoEtherMaxCap > 0);
-            require(_icoEtherMaxCap > _icoEtherMinCap); 
+            require(_bountyMktWallet != 0x0);
+            require(_arbitrationWallet != 0x0);
+            require(_rewardsWallet != 0x0);
+            require(_advisorsWallet != 0x0);            
             
             //set wallet that will receive ether
-            wallet = _wallet;
+            icoWallet = _icoWallet;
 
-            //set wallet            
-            investorsWallet = _investorsWallet;                    
-            workingCapitalWallet = _workingCapitalWallet;                
-            walletToken = _walletToken;                         
-            teamWallet = _teamWallet;                         
-            rewardWallet = _rewardWallet;                        
-            advisior = _advisior; 
+            //set wallet
+            preIcoWallet = _preIcoWallet;
+            teamWallet = _teamWallet;
+            bountyMktWallet = _bountyMktWallet;
+            arbitrationWallet = _arbitrationWallet;
+            rewardsWallet = _rewardsWallet;
+            advisorsWallet = _advisorsWallet;
+            operationWallet = _operationWallet;
 
-            //Set Min and Max EtherCap
-            icoEtherMinCap = _icoEtherMinCap;
-            icoEtherMaxCap = _icoEtherMaxCap;
-
-            //Distribution Token            
-            balances[_investorsWallet] = totalSupply.mul(3).div(100);           //totalSupply * 3%
-            balances[_workingCapitalWallet] = totalSupply.mul(10).div(100);     //totalSupply * 10%      
-            balances[_walletToken] = totalSupply.mul(70).div(100);              //totalSupply * 70%
-            balances[_teamWallet] = totalSupply.mul(10).div(100);               //totalSupply * 10%
-            balances[_rewardWallet] = totalSupply.mul(5).div(100);              //totalSupply * 5%
-            balances[_advisior] = totalSupply.mul(2).div(100);      
+            //Distribution Token  
+            balances[icoWallet] = totalSupply.mul(63).div(100);                 //totalSupply * 63%
+            balances[preIcoWallet] = totalSupply.mul(7).div(100);               //totalSupply * 7%
+            balances[teamWallet] = totalSupply.mul(10).div(100);                //totalSupply * 10%
+            balances[bountyMktWallet] = totalSupply.mul(7).div(100);            //totalSupply * 7%
+            balances[arbitrationWallet] = totalSupply.mul(5).div(100);          //totalSupply * 5%
+            balances[rewardsWallet] = totalSupply.mul(5).div(100);              //totalSupply * 5%
+            balances[advisorsWallet] = totalSupply.mul(2).div(100);             //totalSupply * 2%
+            balances[operationWallet] = totalSupply.mul(1).div(100);            //totalSupply * 1%      
 
             //set pause
             pause();
 
             //set token to sale
-            totalTokenToSale = balances[_walletToken];           
+            totalTokenToSale = balances[icoWallet].add(balances[preIcoWallet]);           
     }
 
  // ======================================== Modifier ==================================================
 
-    modifier acceptsFunds() {        
-        require(icoStartTimestamp != 0 && now >= icoStartTimestamp);          
-        require(now <= icoEndTimestamp || totalRaised < icoEtherMinCap);        
-        require(totalRaised < icoEtherMaxCap);
+    modifier acceptsFunds() {   
+        if (icoStage == 0) {
+            require(now >= icoStartTimestampStage);          
+            require(now <= icoEndTimestampStage); 
+        }
+
+        if (icoStage == 1) {
+            require(now >= icoStartTimestampStage1);          
+            require(now <= icoEndTimestampStage1);            
+        }
+
+        if (icoStage == 2) {
+            require(now >= icoStartTimestampStage2);          
+            require(now <= icoEndTimestampStage2);            
+        }
+
+        if (icoStage == 3) {
+            require(now >= icoStartTimestampStage3);          
+            require(now <= icoEndTimestampStage3);            
+        }
+
+        if (icoStage == 4) {
+            require(now >= icoStartTimestampStage4);          
+            require(now <= icoEndTimestampStage4);            
+        }             
+               
         _;
     }    
 
@@ -118,7 +150,7 @@ contract JubsICO is StandardToken {
 
     }
 
-    modifier testPauseEmergence {
+    modifier PauseEmergence {
         require(!pauseEmergence);
         _;
     }
@@ -126,8 +158,8 @@ contract JubsICO is StandardToken {
 
 //========================================== Functions ===========================================================================
 
-     /// fallback function to buy tokens
-    function () testPauseEmergence nonZeroBuy acceptsFunds payable public {  
+    /// fallback function to buy tokens
+    function buy() PauseEmergence nonZeroBuy acceptsFunds payable public {  
         uint256 amount = msg.value.mul(rate);
 
         assignTokens(msg.sender, amount);
@@ -136,8 +168,8 @@ contract JubsICO is StandardToken {
         forwardFundsToWallet();
     } 
 
-    function forwardFundsToWallet() internal {
-        wallet.transfer(msg.value); // immediately send Ether to wallet address, propagates exception if execution fails
+    function forwardFundsToWallet() internal {        
+        walletETH.transfer(msg.value);              // immediately send Ether to wallet address, propagates exception if execution fails
     }
 
     function assignTokens(address recipient, uint256 amount) internal {        
@@ -152,16 +184,23 @@ contract JubsICO is StandardToken {
         }
         
         Transfer(0x0, recipient, amount);
-    }
+    }  
+    
 
     function manuallyAssignTokens(address recipient, uint256 amount) public onlyOwner {
         require(tokensSold < totalSupply);
         assignTokens(recipient, amount);
     }
 
-    function setRate(uint256 _rate) public onlyOwner {
-        require(!isIcoFinished());
+    function setRate(uint256 _rate) public onlyOwner { 
+        require(_rate < 0);               
         rate = _rate;        
+    }
+
+    function setIcoStage(uint256 _icoStage) public onlyOwner {    
+        require(_icoStage <= 0); 
+        require(_icoStage >= 4);             
+        icoStage = _icoStage;        
     }
 
     function setPauseEmergence() public onlyOwner {        
@@ -170,19 +209,14 @@ contract JubsICO is StandardToken {
 
     function setUnPauseEmergence() public onlyOwner {        
         pauseEmergence = false;
-    }
+    }   
 
-    function isIcoFinished() public constant returns (bool icoFinished) {
-        return (totalRaised >= icoEtherMinCap && icoEndTimestamp != 0 && now > icoEndTimestamp) || (totalRaised >= icoEtherMaxCap);
-    }
-
-    function sendTokenTeam(address _to, uint256 amount) public onlyOwner returns (bool sendTokenTeam) {
+    function sendTokenTeam(address _to, uint256 amount) public onlyOwner {
         require(_to != 0x0);
 
         //test deadline to request token
         require(now >= teamEndTimestamp);
         assignTokens(_to, amount);
-
     }
 
     function burn(uint256 _value) public whenNotPaused {
---
+++ test/metacoin.js
@@ -0,0 +1,13 @@
+var JUBSIco = artifacts.require("./JubsICO.sol");
+
+contract('JubsICO', function(accounts) {  
+  it("should put 10000 JubsICO in the first account", function() {
+    return JubsICO.deployed().then(function(instance) {
+      return instance.getBalance.call(accounts[0]);
+    }).then(function(balance) {   
+    
+     
+      buy.equal(balance.valueOf(), 10000, "10000 wasn't in the first account");
+    });
+  });  
+});
